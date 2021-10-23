49d092d1866c277cb93b3584940c7d7242a36e87
arsle <arsle@LAPTOP-AR77M1VN>
2021-10-24 00:12:43 +0300

    Добавьте файлы проекта.

---
+++ Zadanie20.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Zadanie20
+    {
+        delegate double MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            double R;
+            MyDelegate myDelegate1=OkruS;
+            myDelegate1(R=5);
+
+            MyDelegate myDelegate2 = OkruL;
+            myDelegate2(R = 5);
+
+            MyDelegate myDelegate3 = SharV;
+            myDelegate3(R = 5);
+        }
+
+    static double OkruS(double R)
+        {
+            Console.WriteLine("Введите радиус окружности:");
+            
+            return Math.PI * R * R;
+        }
+
+    static double OkruL(double R)
+        {
+            Console.WriteLine("Введите радиус окружности:");
+            
+            return 2*Math.PI * R;
+        }
+
+    static double SharV(double R)
+        {
+            Console.WriteLine("Введите радиус шара:");
+            
+            return (4/3)*Math.PI * Math.Pow(R,3);
+        }
+
+    }
+
+}
